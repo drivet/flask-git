f4fb7e27bd3acaccf908dc8def4c0f21c9a28dc1
Desmond Rivet <desmond.rivet@gmail.com>
2015-03-12 18:29:50 -0400

    change version to 0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Git',
-    version='1.0',
+    version='0.1',
     url='https://github.com/drivet/flask-git/',
     license='MIT',
     author='Desmond Rivet',
